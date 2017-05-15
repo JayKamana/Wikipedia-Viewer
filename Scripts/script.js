ed12e6b1514b691de62eb9f225e359cdb6f84063
James Kamana <jaykamana@gmail.com>
2017-05-15 15:29:05 +0300

    deploy wikipedia viewer 1.0

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,55 +2,61 @@ $(function(){
 
     var $panel = $('.panel-group');
     var $result = $('.result');
-    $('button').on('click', function(e){
-     e.preventDefault();
-     $panel.html("");
-     var searchTerm = $('#srch-term').val();
-      var count = 1;
+    $('.wiki-search').on('click', function(e){
+       e.preventDefault();
+       $panel.html("");
+       var searchTerm = $('#srch-term').val();
+       var count = 1;
 
-      $.ajax({
+       $.ajax({
 
-         url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchTerm+'&format=json&callback=?',
-         type: 'GET',
-         dataType: 'jsonp',
-         contentType: 'application/json; charset=utf-8',
-         async: false,
+           url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+searchTerm+'&format=json&callback=?',
+           type: 'GET',
+           dataType: 'jsonp',
+           contentType: 'application/json; charset=utf-8',
+           async: false,
 
-         success: function(data){
-                
+           success: function(data){
+            
             for(var i = 0; i < data[1].length; i++){
 
-                $panel.append("<div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'>\
+                if(i==0) {
+                    $panel.append("<div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'>\
+                        <a data-toggle='collapse' data-parent='#accordion' href='#accordion-"+count+"'>"+data[1][i]+"</a>\
+                        </h4>\
+                        </div>\
+                        <div id='accordion-"+count+"' class='panel-collapse collapse-in'>\
+                        <div class='panel-body'>"+data[2][i]+"<p><a href='"+data[3][i]+"'target='_blank'>Read More</a></p>\
+                        </div>\
+                        </div>\
+                        </div>");
+                }
+
+                else {
+                   $panel.append("<div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'>\
                     <a data-toggle='collapse' data-parent='#accordion' href='#accordion-"+count+"'>"+data[1][i]+"</a>\
-                            </h4>\
-                        </div>\
-                        <div id='accordion-"+count+"' class='panel-collapse collapse'>\
-                            <div class='panel-body'>"+data[2][i]+"<p><a href='"+data[3][i]+"'target='_blank'>Read More</a></p>\
-                            </div>\
-                        </div>\
+                    </h4>\
+                    </div>\
+                    <div id='accordion-"+count+"' class='panel-collapse collapse'>\
+                    <div class='panel-body'>"+data[2][i]+"<p><a href='"+data[3][i]+"'target='_blank'>Read More</a></p>\
+                    </div>\
+                    </div>\
                     </div>");
-                        
-                count++;                              
-            }
-        }
+               }
+               
+               
+               count++;                              
+           }
+       }
 
-      })
+   })
 
-      
+       
 
+   })
+    
+    
 })
-        // $result.html("<p>All new content. <em>You bet!</em></p>");
-        // $result.html('div class="panel panel-default"></div>');
-                    
- })
 
 
 
-        // <a data-toggle='collapse' data-parent='#accordion' href='#accordion-one>Harry Potter</a>\
-        //                     </h4>\
-        //                 </div>\
-        //                 <div id='accordion-one' class='panel-collapse collapse in'>\
-        //                     <div class='panel-body'>lorem ipsum <p><a href='#'>Read More</a></p>\
-        //                     </div>\
-        //                 </div>\
-        //             </div>");
